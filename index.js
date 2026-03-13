d2cd283e5388302e05e18cdf087436391167f0dc
UrielGalanti <49553172@est.ort.edu.ar>
2026-03-13 12:13:21 -0300

    Prueba consola y modulo matemática

---
+++ index.js
@@ -0,0 +1,11 @@
+//1
+// let nombre = "Messi"
+// console.log("hola soy " + nombre)
+// console.log(`hola soy ${nombre}`)
+
+//2
+import * as matematica from './src/modules/matematica.js'
+console.log(matematica.resta(8, 3))
+console.log(matematica.division(8, 3))
+console.log(matematica.suma(8, 3))
+console.log(matematica.multiplicarPorPi(8))
---
+++ src/modules/matematica.js
@@ -0,0 +1,11 @@
+//2
+const suma = (num1, num2) => num1 + num2
+const division = (num1, num2) => num1 / num2
+
+export function multiplicarPorPi (num){
+    return num * 3.1416
+}
+export function resta(num1, num2){
+    return num1 - num2
+}
+export {division, suma}
