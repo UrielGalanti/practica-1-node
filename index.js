d1e7a72fbe4e40c2a7cb953d9c66576aa04d97b4
UrielGalanti <49553172@est.ort.edu.ar>
2026-03-16 08:21:00 -0300

    ejercicios 3 y 5

--- index.js
+++ index.js
@@ -4,8 +4,20 @@
 // console.log(`hola soy ${nombre}`)
 
 //2
-import * as matematica from './src/modules/matematica.js'
-console.log(matematica.resta(8, 3))
-console.log(matematica.division(8, 3))
-console.log(matematica.suma(8, 3))
-console.log(matematica.multiplicarPorPi(8))
+// import * as matematica from './src/modules/matematica.js'
+// console.log(matematica.resta(8, 3))
+// console.log(matematica.division(8, 3))
+// console.log(matematica.suma(8, 3))
+// console.log(matematica.multiplicarPorPi(8))
+
+//3
+// import Alumno from './src/modules/alumno.js'
+// const usuario1 = new Alumno("Juan", "12345678")
+// const usuario2 = new Alumno("Sofia", "87654321")
+// console.log(usuario1.username, usuario1.DNI)
+// console.log(usuario2.username, usuario2.DNI)
+
+//5
+import {parsearUrl} from './src/modules/url.js'
+let objeto = parsearUrl("http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo"); 
+console.log(objeto);
---
+++ src/modules/alumno.js
@@ -0,0 +1,9 @@
+//3
+class Alumno {
+
+    constructor(username, dni){
+        this.username = username
+        this.DNI = dni
+    }
+}
+export default Alumno;
